75df33f72613d4a61a7c85efd5e2170034738d18
Timothy Keating <timothykeating@gmail.com>
2021-12-09 11:33:24 -0500

    uploading single_points work

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -23,9 +23,11 @@ let circle = L.circleMarker([34.0522,-118.2437], {
 //   //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// let marker2 = L.circle([34.0522, -118.2437], {
-//   radius: 100
-// }).addTo(map);
+let marker2 = L.circle([34.0522, -118.2437], {
+  radius: 100
+}).addTo(map);
+
+
 
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -38,4 +40,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
+
+
 
